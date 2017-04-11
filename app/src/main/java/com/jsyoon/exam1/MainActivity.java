a94646904f0a78ba619a5cb1cc4473537c1351f0
jsyoon <jsyoon@shinwooend.co.kr>
2017-04-11 11:09:03 +0900

    Main Activity code 수정

--- app/src/main/java/com/jsyoon/exam1/MainActivity.java
+++ app/src/main/java/com/jsyoon/exam1/MainActivity.java
@@ -2,12 +2,41 @@ package com.jsyoon.exam1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                submitOrder(v);
+            }
+        });
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
